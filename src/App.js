dd3c98b929a3f7b77effb4245ddb58721467923d
Adriel <adrielalegre721@gmail.com>
2022-07-25 22:47:27 -0300

    tercera entrega

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/Container/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
-const App =()=> {
+export default function App() {
   return (
         <div className="row">
           <div className="col">
             <NavBar/>
           </div>
+          <div className="container">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <ItemListContainer getting="Bienvenidos!"></ItemListContainer>
+            </div>
+          </div>
+          </div>
         </div>
   );
 }
 
-export default App;
 
