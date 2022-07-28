39660efd00e64e67c067bf06d856bb6763fec89c
Adriel <adrielalegre721@gmail.com>
2022-07-28 12:15:14 -0300

    se agrego componente cartwidget

--- src/components/Container/NavBar.js
+++ src/components/Container/NavBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Nav.css";
+import "../CartWidget/CartWidget";
+import CartWidget from "../CartWidget/CartWidget";
 
-const NavBar =()=>{
+export default function NavBar (){
     return(
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Fnx Hardware</a>
             <div className="row contendor-icono">
                 <div className="col-4 text-start contendor-icono">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
-                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
-                    </svg>
+                    <CartWidget/>
                 </div>
                 <div className="col-4">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,5 +35,3 @@ const NavBar =()=>{
     </nav>
     );
 }
-
-export default NavBar;
